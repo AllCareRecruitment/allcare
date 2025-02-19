2be96165f86c38de8858e6ce08e550dbfdc40f26
ZylnGovender <zylng@runninghill.co.za>
2025-02-19 09:50:30 +0200

    Made the registration context global

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { LoadingProvider } from './(ui)/services/context/LoadingContext'
 import Spinner from './(ui)/components/spinner'
-
+import { RegistrationProvider } from './(ui)/services/context/RegistrationContext'
 export default function RootLayout({
     children,
 }: Readonly<{
@@ -11,9 +11,11 @@ export default function RootLayout({
         <html lang="en">
             <body>
                 <LoadingProvider>
-                    <Spinner />
-                    <p>hello</p>
-                    {children}
+                    <RegistrationProvider>
+                        <Spinner />
+                        <p>hello</p>
+                        {children}
+                    </RegistrationProvider>
                 </LoadingProvider>
             </body>
         </html>
