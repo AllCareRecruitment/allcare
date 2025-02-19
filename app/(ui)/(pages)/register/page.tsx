484d61b68d00eb43e184a3ab9abce9425189fc36
ZylnGovender <zylng@runninghill.co.za>
2025-02-19 12:03:36 +0200

    added colors to global styling sheet

--- app/(ui)/(pages)/register/page.tsx
+++ app/(ui)/(pages)/register/page.tsx
@@ -14,63 +14,28 @@ const RegistrationSelection = () => {
 
     return (
         <div className="flex flex-col md:flex-row h-screen">
-            <div className="flex-1
-            flex
-            flex-col
-            justify-center
-            items-center
-            bg-[#6B7165]
-            text-white
-            p-10
-            w-full
-            md:w-1/2">
+            <div className="flex-1 flex flex-col justify-center
+            items-center bg-jobseekers text-light p-10 w-full md:w-1/2">
                 <h2 className="text-3xl font-bold">FOR JOB SEEKERS</h2>
                 <p className="text-lg mt-2">{'Let\'s get you Hired'}</p>
-                <h1 className="text-5xl font-bold text-[#D7D3AC] mt-2">HIRED</h1>
-                <button
-                    className="mt-6
-                    bg-[#FFC7C9]
-                    text-gray-800
-                    font-semibold
-                    py-2
-                    px-6
-                    rounded-md
-                    hover:bg-[#F5BABA]
-                    transition
-                    flex
-                    items-center"
-                    onClick={() => handleButtonClick('Applicant')}
+                <h1 className="text-5xl font-bold text-highlight mt-2">HIRED</h1>
+                <button className="mt-6 bg-button text-button
+                    font-semibold py-2 px-6 rounded-md
+                    bg-button-hover transition flex items-center"
+                onClick={() => handleButtonClick('Applicant')}
                 >
                     GET STARTED <span className="ml-2">→</span>
                 </button>
             </div>
 
-            <div className="flex-1
-            flex
-            flex-col
-            justify-center
-            items-center
-            bg-[#D7D3AC]
-            text-white
-            p-10
-            w-full
-            md:w-1/2">
+            <div className="flex-1 flex flex-col justify-center
+                    items-center bg-employers text-light p-10 w-full md:w-1/2">
                 <h2 className="text-3xl font-bold">FOR EMPLOYERS</h2>
                 <p className="text-lg mt-2">Build your dream</p>
-                <h1 className="text-5xl font-bold text-[#6B7165] mt-2">TEAM</h1>
-                <button
-                    className="mt-6
-                    bg-[#FFC7C9]
-                    text-gray-800
-                    font-semibold
-                    py-2
-                    px-6
-                    rounded-md
-                    hover:bg-[#F5BABA]
-                    transition
-                    flex
-                    items-center"
-                    onClick={() => handleButtonClick('Employer')}
+                <h1 className="text-5xl font-bold text-highlight-dark mt-2">TEAM</h1>
+                <button className="mt-6 bg-button text-button font-semibold py-2 px-6
+                    rounded-md bg-button-hover transition flex items-center"
+                onClick={() => handleButtonClick('Employer')}
                 >
                     GET STARTED <span className="ml-2">→</span>
                 </button>
