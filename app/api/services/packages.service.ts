621a89e08e7debafd967fa82e03f8d3579f8ee22
ZylnGovender <zylng@runninghill.co.za>
2025-03-03 14:41:15 +0200

    resolved comments

--- app/api/services/packages.service.ts
+++ app/api/services/packages.service.ts
@@ -1,5 +1,10 @@
 import { getPackagesByRoleId } from '@/app/api/repository/packages.repository'
 
 export async function fetchPackagesForRole(roleId: number) {
-    return getPackagesByRoleId(roleId)
+    const data = await getPackagesByRoleId(roleId)
+
+    if (!data) {
+        throw new Error('Failed to get packages for role')
+    }
+    return data
 }
