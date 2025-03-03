f0ba0b49dda5ada124ae3d6585edadf73c62e151
Dean Schutte <deans@runninghill.co.za>
2025-03-03 07:49:40 +0200

    update home layout

---
+++ app/(ui)/components/navbar/navbar.component.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { MenuItems } from '@/app/(ui)/models/menu.model'
+import Image from 'next/image'
+
+interface HeaderProps {
+    menuItems: MenuItems[];
+}
+
+const Navbar: React.FC<HeaderProps> = ({ menuItems }) => {
+    return (
+        <header className="bg-jobseekers text-white p-4 grid grid-cols-2">
+            {/*<Image src="/logo.png" alt="Logo" width={100} height={100} layout="responsive" />*/}
+            <h1 className="text-2xl font-bold">All Care Recruitment</h1>
+            <nav className="flex justify-end">
+                {
+                    menuItems.map((item) =>
+                        <a key={item.name} href={item.url} className="text-white hover:underline p-2">
+                            {item.name}
+                        </a>
+                    )
+                }
+            </nav>
+
+        </header>
+    )
+}
+
+export default Navbar
