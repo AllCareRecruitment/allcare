44993f06c79b3d4100b06dee26b2e7768251b9fa
ZylnGovender <zylng@runninghill.co.za>
2025-03-04 14:38:47 +0200

    added spinner, fixed issue where you'd see content for a short while before spinner activated

--- app/(ui)/(pages)/register/package-selection/page.tsx
+++ app/(ui)/(pages)/register/package-selection/page.tsx
@@ -3,6 +3,7 @@
 import { getPackagesByRoleId } from '@/app/(ui)/services/packagesService'
 import { useEffect, useState, useRef } from 'react'
 import styles from './styles.module.css'
+import { useLoading } from '@/app/(ui)/context/LoadingContext'
 
 interface Package {
     planName: string;
@@ -15,17 +16,25 @@ export default function PricingPage() {
     const scrollContainerRef = useRef<HTMLDivElement>(null)
     const [showLeftArrow, setShowLeftArrow] = useState(false)
     const [showRightArrow, setShowRightArrow] = useState(false)
+    const { setLoading } = useLoading()
+    const [isLocalLoading, setIsLocalLoading] = useState(true)
 
     useEffect(() => {
         (async () => {
+            setLoading(true)
+            setIsLocalLoading(true)
+
             try {
                 const response = await getPackagesByRoleId(2)
                 setPackages(response.packages)
             } catch (error) {
-                console.error('Error fetching packages:', error)
+                return error
+            } finally {
+                setLoading(false)
+                setIsLocalLoading(false)
             }
         })()
-    }, [])
+    }, [setLoading])
 
     useEffect(() => {
         if (packages.length > 3) {
@@ -99,76 +108,78 @@ export default function PricingPage() {
 
     return (
         <div className="min-h-screen bg-selectPlanBackground flex flex-col items-center justify-center p-6">
-            <div className="w-full max-w-5xl">
-                <div className={dynamicPlans.length === 1 ? 'text-center' : 'text-left'}>
-                    <h1 className="text-3xl font-bold text-gray-700 mb-8">Find The Right Plan.</h1>
-                </div>
+            {isLocalLoading ? null : // If loading, render nothing inside the outer div
+                <div className="w-full max-w-5xl">
+                    <div className={dynamicPlans.length === 1 ? 'text-center' : 'text-left'}>
+                        <h1 className="text-3xl font-bold text-gray-700 mb-8">Find The Right Plan.</h1>
+                    </div>
 
-                <div className="relative">
-                    {showLeftArrow && 
-                        <button
-                            onClick={scrollLeft}
-                            className="absolute left-0 top-1/2 transform -translate-y-1/2 -ml-6 z-10 bg-white
-                            rounded-full p-2 shadow-md hover:bg-gray-100" aria-label="Scroll left"
-                        >
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                                fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round"
-                                strokeLinejoin="round">
-                                <path d="M15 18l-6-6 6-6" />
-                            </svg>
-                        </button>
-                    }
-
-                    {showRightArrow && 
-                        <button
-                            onClick={scrollRight}
-                            className="absolute right-0 top-1/2 transform -translate-y-1/2 -mr-6 z-10 bg-white
-                            rounded-full p-2 shadow-md hover:bg-gray-100" aria-label="Scroll right"
-                        >
-                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                                fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round"
-                                strokeLinejoin="round">
-                                <path d="M9 18l6-6-6-6" />
-                            </svg>
-                        </button>
-                    }
-
-                    <div
-                        ref={scrollContainerRef}
-                        className={getContainerClass()}
-                        onScroll={handleScroll}
-                    >
-                        {dynamicPlans.map((plan, index) =>
-                            <div
-                                key={index}
-                                className={`${getPackageWidthClass()} rounded-lg shadow-lg flex flex-col 
-                                items-center ${plan.bgColor} overflow-hidden`}
+                    <div className="relative">
+                        {showLeftArrow &&
+                            <button
+                                onClick={scrollLeft}
+                                className="absolute left-0 top-1/2 transform -translate-y-1/2 -ml-6 z-10 bg-white
+                                rounded-full p-2 shadow-md hover:bg-gray-100" aria-label="Scroll left"
                             >
-                                <div className={`w-full p-4 text-center ${plan.titleBgColor}`}>
-                                    <h2 className="text-xl font-bold text-white">{plan.name}</h2>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                    fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round"
+                                    strokeLinejoin="round">
+                                    <path d="M15 18l-6-6 6-6" />
+                                </svg>
+                            </button>
+                        }
+
+                        {showRightArrow &&
+                            <button
+                                onClick={scrollRight}
+                                className="absolute right-0 top-1/2 transform -translate-y-1/2 -mr-6 z-10 bg-white
+                                rounded-full p-2 shadow-md hover:bg-gray-100" aria-label="Scroll right"
+                            >
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                    fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round"
+                                    strokeLinejoin="round">
+                                    <path d="M9 18l6-6-6-6" />
+                                </svg>
+                            </button>
+                        }
+
+                        <div
+                            ref={scrollContainerRef}
+                            className={getContainerClass()}
+                            onScroll={handleScroll}
+                        >
+                            {dynamicPlans.map((plan, index) =>
+                                <div
+                                    key={index}
+                                    className={`${getPackageWidthClass()} rounded-lg shadow-lg flex flex-col 
+                                    items-center ${plan.bgColor} overflow-hidden`}
+                                >
+                                    <div className={`w-full p-4 text-center ${plan.titleBgColor}`}>
+                                        <h2 className="text-xl font-bold text-white">{plan.name}</h2>
+                                    </div>
+
+                                    <div className="flex flex-col flex-grow w-full items-center p-6">
+                                        <ul className="mb-6">
+                                            {plan.description.split(', ').map((feature, idx) =>
+                                                <li key={idx} className="text-white flex items-center gap-2 mb-2">
+                                                    <span className="text-lg">✔</span> {feature}
+                                                </li>
+                                            )}
+                                        </ul>
+
+                                        <div className="flex-grow"></div>
+
+                                        <button className="bg-planGetStartedButton text-white px-4 py-2
+                                        rounded-md hover:bg-[#6d664a] transition mt-auto">
+                                            Get Started
+                                        </button>
+                                    </div>
                                 </div>
-
-                                <div className="flex flex-col flex-grow w-full items-center p-6">
-                                    <ul className="mb-6">
-                                        {plan.description.split(', ').map((feature, idx) =>
-                                            <li key={idx} className="text-white flex items-center gap-2 mb-2">
-                                                <span className="text-lg">✔</span> {feature}
-                                            </li>
-                                        )}
-                                    </ul>
-
-                                    <div className="flex-grow"></div>
-
-                                    <button className="bg-planGetStartedButton text-white px-4 py-2
-                                    rounded-md hover:bg-[#6d664a] transition mt-auto">
-                                        Get Started
-                                    </button>
-                                </div>
-                            </div>
-                        )}
+                            )}
+                        </div>
                     </div>
                 </div>
-            </div>
+            }
         </div>
     )
 }
